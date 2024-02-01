2879802db4e4986200c90f977a397d438269dacd
rryyii <ryanyi455@gmail.com>
2024-02-01 02:40:18 -0500

    Cleaned a little bit of the code/imports/etc

--- src/main/java/fitrack_proj/app/Fitrack.java
+++ src/main/java/fitrack_proj/app/Fitrack.java
@@ -1,33 +1,32 @@
 package fitrack_proj.app;
 
-import javax.swing.JFrame;
-
 import fitrack_proj.panel.LoginPanel;
 import fitrack_proj.util.FitrackDatabase;
+
+import javax.swing.*;
 
 /**
  * Primary Fittrack Functionality Class.
  *
  * @author yirw
- *
  */
 public class Fitrack {
-	
-	/**
-	 * Creates an instance of Fitrack application.
-	 */
-	public void createInstance() {
-		LoginPanel menu = new LoginPanel();
-		JFrame gui = menu.createMenu(new FitrackDatabase());
-	}
-	
-	/**
-	 * Main Method.
-	 *
-	 * @param args Command-line arguments
-	 */
-	public static void main(String[] args) {
-		Fitrack instance = new Fitrack();
-		instance.createInstance();
-	}
+
+  /**
+   * Main Method.
+   *
+   * @param args Command-line arguments
+   */
+  public static void main(String[] args) {
+    Fitrack instance = new Fitrack();
+    instance.createInstance();
+  }
+
+  /**
+   * Creates an instance of Fitrack application.
+   */
+  public void createInstance() {
+    LoginPanel menu = new LoginPanel();
+    JFrame gui = menu.createMenu(new FitrackDatabase());
+  }
 }
--- src/main/java/fitrack_proj/app/NutritionTrack.java
+++ src/main/java/fitrack_proj/app/NutritionTrack.java
@@ -1,19 +1,17 @@
 package fitrack_proj.app;
 
-import java.awt.CardLayout;
-
-import javax.swing.JPanel;
-
 import org.knowm.xchart.PieChart;
 import org.knowm.xchart.SwingWrapper;
 import org.knowm.xchart.XChartPanel;
 
+import javax.swing.*;
+
 public class NutritionTrack extends JPanel {
-	
-	public void displayChart() {
-		PieChart chart = new PieChart(5, 5);
-		chart.addSeries("", 50);
-		XChartPanel<PieChart> chartPanel = new SwingWrapper<>(chart).getXChartPanel();
-		this.add(chartPanel);
-	}
+
+  public void displayChart() {
+    PieChart chart = new PieChart(5, 5);
+    chart.addSeries("", 50);
+    XChartPanel<PieChart> chartPanel = new SwingWrapper<>(chart).getXChartPanel();
+    this.add(chartPanel);
+  }
 }
