2db70de05290a0c4b14b6906a39f87800db15397
rryyii <ryanyi455@gmail.com>
2024-02-08 02:19:11 -0500

    Changed some sizing issues, etc

--- src/main/java/fitrack_proj/panel/LoginPanel.java
+++ src/main/java/fitrack_proj/panel/LoginPanel.java
@@ -23,7 +23,7 @@ import java.awt.event.ActionListener;
 import java.util.Objects;
 
 /**
- * Primary JSwing Class.
+ * Main panel that contains login and register functions.
  *
  * @author yirw
  */
@@ -45,96 +45,98 @@ public class LoginPanel {
 		JMenuBar menuBar = new JMenuBar();
 		frame.setJMenuBar(menuBar);
 		final JPanel cardPanel = new JPanel(new CardLayout());
-		final JPanel mainScreen = new JPanel();
-		dashPanel = new DashPanel(cardPanel, connection);
-		final JButton login = new JButton();
-		login.setText("Login");
-		login.addActionListener(new ActionListener() {
+		final JPanel mainPanel = new JPanel();
+		dashPanel = new DashPanel(cardPanel, connection, frame);
+		final JButton loginButton = new JButton();
+		loginButton.setText("Login");
+		// Creates the ActionListener for the login button
+		loginButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				SwingUtilities.invokeLater(new Runnable() {
 					@Override
 					public void run() {
 						dashPanel.removeAll();
-						GridLayout loginLay = new GridLayout(2, 2);
-						loginLay.setHgap(10);
-						loginLay.setVgap(10);
-						JTextField username = new JTextField(10);
-						JTextField password = new JPasswordField(10);
-						JPanel userLogin = new JPanel(loginLay);
+						GridLayout loginLayout = new GridLayout(2, 2);
+						loginLayout.setHgap(10);
+						loginLayout.setVgap(10);
+						JTextField usernameField = new JTextField(10);
+						JTextField passwordField = new JPasswordField(10);
+						JPanel userLogin = new JPanel(loginLayout);
 						userLogin.add(new JLabel("Username:"));
-						userLogin.add(username);
+						userLogin.add(usernameField);
 						userLogin.add(Box.createHorizontalStrut(15));
 						userLogin.add(new JLabel("Password:"));
-						userLogin.add(password);
+						userLogin.add(passwordField);
 						int result = JOptionPane.showConfirmDialog(null, userLogin,
-								"Please enter your username and password.", JOptionPane.OK_CANCEL_OPTION);
+								"Please enter your Username and Password.", JOptionPane.OK_CANCEL_OPTION);
 						if (result == JOptionPane.CANCEL_OPTION) {
-						} else if ((username.getText().isEmpty() || password.getText().isEmpty())) {
-							JOptionPane.showMessageDialog(login, "Please fill in all fields.");
-						} else if (!connection.verifyLogin(username.getText(), password.getText())) {
-							JOptionPane.showMessageDialog(login, "Invalid username and password.");
+						} else if ((usernameField.getText().isEmpty() || passwordField.getText().isEmpty())) {
+							JOptionPane.showMessageDialog(loginButton, "Please fill in all fields.");
+						} else if (!connection.verifyLogin(usernameField.getText(), passwordField.getText())) {
+							JOptionPane.showMessageDialog(loginButton, "Invalid Username and Password.");
 						} else {
-							dashPanel.setUser(username.getText(), password.getText());
+							dashPanel.setUser(usernameField.getText(), passwordField.getText());
 							dashPanel.showPanel();
 							((CardLayout) cardPanel.getLayout()).show(cardPanel, "dash");
+							frame.pack();
 						}
 					}
 				});
 			}
 		});
 
-		final JButton register = new JButton();
-		register.setText("Register");
-		register.addActionListener(e -> {
+		final JButton registerButton = new JButton();
+		registerButton.setText("Register");
+		registerButton.addActionListener(e -> {
 			SwingUtilities.invokeLater(new Runnable() {
 				@Override
 				public void run() {
-					GridLayout registerLay = new GridLayout(7, 2);
-					registerLay.setVgap(10);
-					registerLay.setHgap(10);
-					JTextField username = new JTextField(15);
-					JTextField height = new JTextField(10);
-					JTextField password = new JTextField(10);
+					GridLayout registerLayout = new GridLayout(7, 2);
+					registerLayout.setVgap(10);
+					registerLayout.setHgap(10);
+					JTextField usernameField = new JTextField(15);
+					JTextField heightField = new JTextField(10);
+					JTextField passwordField = new JTextField(10);
 					JTextField weight = new JTextField(10);
 					JTextField age = new JTextField((10));
-					JComboBox<String> gender = new JComboBox<>();
-					gender.addItem("");
-					gender.addItem("Male");
-					gender.addItem("Female");
-					gender.addItem("Other");
-					// Choose activity level
-					JComboBox<String> activity = new JComboBox<>();
-					activity.addItem("");
-					activity.addItem("Little to no exercise");
-					activity.addItem("Moderate exercise");
-					activity.addItem("Active");
-					activity.addItem("Very Active");
+					JComboBox<String> genderBox = new JComboBox<>();
+					genderBox.addItem("");
+					genderBox.addItem("Male");
+					genderBox.addItem("Female");
+					genderBox.addItem("Other");
+					// Choose Activity level
+					JComboBox<String> activityBox = new JComboBox<>();
+					activityBox.addItem("");
+					activityBox.addItem("Little to no exercise");
+					activityBox.addItem("Moderate exercise");
+					activityBox.addItem("Active");
+					activityBox.addItem("Very Active");
 					// Registration details
-					JPanel userRegister = new JPanel();
-					userRegister.setLayout(registerLay);
-					userRegister.add(new JLabel(" Username:"));
-					userRegister.add(username);
-					userRegister.add(new JLabel("Password:"));
-					userRegister.add(password);
-					userRegister.add(new JLabel("Gender:"));
-					userRegister.add(gender);
-					userRegister.add(new JLabel("Age:"));
-					userRegister.add(age);
-					userRegister.add(new JLabel("Height (Inches):"));
-					userRegister.add(height);
-					userRegister.add(new JLabel("Weight (Lbs):"));
-					userRegister.add(weight);
-					userRegister.add(new JLabel("Activity Level:"));
-					userRegister.add(activity);
+					JPanel registerPanel = new JPanel();
+					registerPanel.setLayout(registerLayout);
+					registerPanel.add(new JLabel(" Username:"));
+					registerPanel.add(usernameField);
+					registerPanel.add(new JLabel("Password:"));
+					registerPanel.add(passwordField);
+					registerPanel.add(new JLabel("Gender:"));
+					registerPanel.add(genderBox);
+					registerPanel.add(new JLabel("Age:"));
+					registerPanel.add(age);
+					registerPanel.add(new JLabel("Height (Inches):"));
+					registerPanel.add(heightField);
+					registerPanel.add(new JLabel("Weight (Lbs):"));
+					registerPanel.add(weight);
+					registerPanel.add(new JLabel("Activity Level:"));
+					registerPanel.add(activityBox);
 
-					int result = JOptionPane.showConfirmDialog(null, userRegister, "User Registration",
+					int result = JOptionPane.showConfirmDialog(null, registerPanel, "User Registration",
 							JOptionPane.OK_CANCEL_OPTION);
 					if (result == JOptionPane.CANCEL_OPTION) {
-					} else if (connection.registerUser(username.getText(), password.getText(),
-							Objects.requireNonNull(gender.getSelectedItem()).toString(),
-							Integer.parseInt(weight.getText()), Integer.parseInt(height.getText()),
-							Objects.requireNonNull(activity.getSelectedItem()).toString(),
+					} else if (connection.registerUser(usernameField.getText(), passwordField.getText(),
+							Objects.requireNonNull(genderBox.getSelectedItem()).toString(),
+							Integer.parseInt(weight.getText()), Integer.parseInt(heightField.getText()),
+							Objects.requireNonNull(activityBox.getSelectedItem()).toString(),
 							Integer.parseInt(age.getText())) == -1) {
 						System.out.println("Failed to register user");
 					}
@@ -143,17 +145,17 @@ public class LoginPanel {
 
 		});
 
-		mainScreen.add(login);
-		mainScreen.add(register);
+		mainPanel.add(loginButton);
+		mainPanel.add(registerButton);
 
-		cardPanel.add(mainScreen, "login");
+		cardPanel.add(mainPanel, "login");
 		cardPanel.add(dashPanel, "dash");
-
-		// Misc
+		
 		frame.add(cardPanel);
+		// Adjust frame settings
 		frame.setTitle("Fitrack");
-		frame.setMinimumSize(new Dimension(500, 500));
-		frame.setResizable(false);
+		frame.setMinimumSize(new Dimension(250, 250));
+		frame.setResizable(true);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		frame.pack();
