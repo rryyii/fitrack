05b6fb3bdf9fbda402caf43ea1cd7278cd1e342b
rryyii <ryanyi455@gmail.com>
2024-02-08 02:18:26 -0500

    Added a semi-functional clear history button, need to work on chart compatability

--- src/main/java/fitrack_proj/panel/DashPanel.java
+++ src/main/java/fitrack_proj/panel/DashPanel.java
@@ -1,21 +1,31 @@
 package fitrack_proj.panel;
 
 import fitrack_proj.app.FitnessHistory;
+import fitrack_proj.util.FitUser;
 import fitrack_proj.util.FitrackDatabase;
 import fitrack_proj.util.PasswordEncryptor;
 
 import javax.swing.JPanel;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JTable;
 import javax.swing.JButton;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JComboBox;
+import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.CardLayout;
+import java.awt.Dimension;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
@@ -27,16 +37,10 @@ import java.util.Date;
  */
 public class DashPanel extends JPanel {
 	private static final long serialVersionUID = -2136961495276530281L;
-	private int userId;
-	private int height;
-	private int weight;
-	private int age;
-	private String activity;
-	private String gender;
-	private String username;
 	private final FitrackDatabase connection;
+	private FitUser userInfo;
 	private final JPanel cards;
-	private ResultSet results;
+	private final JFrame frame;
 
 	/**
 	 * DashPanel constructor.
@@ -44,9 +48,10 @@ public class DashPanel extends JPanel {
 	 * @param cards      CardPanel to utilize
 	 * @param connection FitrackDatabase to use for information
 	 */
-	public DashPanel(final JPanel cards, FitrackDatabase connection) {
+	public DashPanel(final JPanel cards, FitrackDatabase connection, JFrame frame) {
 		this.cards = cards;
 		this.connection = connection;
+		this.frame = frame;
 	}
 
 	/**
@@ -56,16 +61,18 @@ public class DashPanel extends JPanel {
 	 * @param password password of the user to set as current
 	 */
 	public void setUser(String username, String password) {
-		results = connection.retrieveUser(username, PasswordEncryptor.hashPassword(password));
-		this.username = username;
+		userInfo = new FitUser();
+		userInfo.setResults(connection.retrieveUser(username, PasswordEncryptor.hashPassword(password)));
+		userInfo.setUsername(username);
+		ResultSet results = userInfo.getResults();
 		try {
-			while (results.next()) {
-				this.userId = results.getInt("user_id");
-				this.height = results.getInt("height");
-				this.weight = results.getInt("weight");
-				this.age = results.getInt("age");
-				this.gender = results.getString("gender");
-				this.activity = results.getString("activity");
+			while (userInfo.getResults().next()) {
+				userInfo.setUserId(results.getInt("user_id"));
+				userInfo.setHeight(results.getInt("height"));
+				userInfo.setWeight(results.getInt("weight"));
+				userInfo.setAge(results.getInt("age"));
+				userInfo.setGender(results.getString("gender"));
+				userInfo.setActivity(results.getString("activity"));
 			}
 		} catch (SQLException sqe) {
 			System.out.println("Failed to set the user to the current DashPanel.");
@@ -81,94 +88,138 @@ public class DashPanel extends JPanel {
 		this.removeAll();
 		this.setLayout(new GridBagLayout());
 		GridBagConstraints consts = new GridBagConstraints();
-		consts.insets = new Insets(5, 5, 5, 5); // Add spacing between components
+		consts.insets = new Insets(5, 5, 5, 5);
 
 		JTextArea status = new JTextArea();
 		status.setEditable(false);
-		status.setText("Welcome back " + username + "!\n" + "Current Height: " + height + " inches\n"
-				+ "Current Weight: " + weight + " lbs\n" + "Projected Daily Calories Burnt: "
-				+ FitnessHistory.calculateCal(weight, height, gender, activity, age) + "\n");
-		consts.gridy = 1;
+		status.setText(
+				"Welcome back " + userInfo.getUsername() + "!\n" + "Current Height: " + userInfo.getHeight()
+						+ " inches\n" + "Current Weight: " + userInfo.getWeight() + " lbs\n"
+						+ "Projected Daily Calories Burnt: " + FitnessHistory.calculateCal(userInfo.getWeight(),
+								userInfo.getHeight(), userInfo.getGender(), userInfo.getActivity(), userInfo.getAge())
+						+ "\n");
+		consts.gridx = 0;
+		consts.gridy = 0;
+		consts.gridwidth = 2;
 		consts.fill = GridBagConstraints.HORIZONTAL; // Make the JTextArea horizontally expandable
 		this.add(status, consts);
 
+		consts.gridwidth = 1;
+		consts.gridy = 1;
 		JLabel selectActivity = new JLabel("Select performed exercise or activity type:");
-		consts.gridy = 2;
-		consts.fill = GridBagConstraints.NONE; // Reset fill to default
 		this.add(selectActivity, consts);
 
+		consts.gridy = 2;
 		final JComboBox<String> addExercise = new JComboBox<>();
 		addExercise.addItem("");
 		addExercise.addItem("Sports");
 		addExercise.addItem("Biking");
 		addExercise.addItem("Conditioning (resistance training)");
 		addExercise.addItem("Cardio");
-		consts.gridy = 3;
 		this.add(addExercise, consts);
 
-		final JTextField timeElapsed = new JTextField(10);
+		consts.gridy = 3;
 		JLabel exerciseTime = new JLabel("Enter elapsed time (in minutes):");
-		consts.gridy = 4;
 		this.add(exerciseTime, consts);
-		consts.gridy = 5;
+		consts.gridy = 4;
+		final JTextField timeElapsed = new JTextField(10);
 		this.add(timeElapsed, consts);
 
-		JLabel history = new JLabel("Exercise History:");
-		consts.gridy = 7;
-		this.add(history, consts);
-		JTextArea historyText = new JTextArea();
-		updateHistory(historyText);
-		consts.gridy = 8;
-		this.add(historyText, consts);
+		consts.gridy = 6;
+		JLabel historyLabel = new JLabel("Exercise History:");
+		this.add(historyLabel, consts);
 
-		JButton registerExercise = new JButton("Submit");
-		registerExercise.addActionListener(e -> {
+		consts.gridy = 7;
+		String column[] = { "Exercise Type", "Duration", "Date" };
+		DefaultTableModel dftm = new DefaultTableModel(column, 0);
+		JTable table = new JTable(dftm);
+		table.setVisible(true);
+		updateHistory(dftm);
+		this.add(table, consts);
+
+		consts.gridy = 5;
+		JButton exerciseButton = new JButton("Submit");
+		exerciseButton.addActionListener(e -> {
 			Object selected = addExercise.getSelectedItem();
 			String selectedText = timeElapsed.getText();
 			assert selected != null;
-			connection.insertExercise(selected.toString(), Integer.parseInt(selectedText), userId);
+			connection.insertExercise(selected.toString(), Integer.parseInt(selectedText), userInfo.getUserId());
 			SwingUtilities.invokeLater(new Runnable() {
 				@Override
 				public void run() {
-					updateHistory(historyText);
+					ResultSet result = updateHistory(dftm);
+					showNutrition.updateChart(result);
+					frame.pack();
 				}
 			});
 		});
-		consts.gridy = 6;
-		this.add(registerExercise, consts);
+		this.add(exerciseButton, consts);
 
-		final JPanel nutritionPanel = showNutrition.createChart(retrieveHistory(connection, userId));
+		consts.gridx = 0;
+		consts.gridy = 8;
+		consts.gridheight = 1;
+		consts.gridwidth = 2;
+		consts.fill = GridBagConstraints.HORIZONTAL;
+		final JPanel nutritionPanel = showNutrition.createChart(retrieveHistory(connection, userInfo.getUserId()),
+				this.frame);
 		JButton chartButton = new JButton("Show Chart");
 		chartButton.addActionListener(e -> {
 			SwingUtilities.invokeLater(new Runnable() {
 				@Override
 				public void run() {
 					((CardLayout) cards.getLayout()).show(cards, "nutrition");
+					frame.setSize(new Dimension(500, 500));
 				}
 			});
 		});
-		consts.gridx = 0;
-		consts.gridy = 0;
 		this.add(chartButton, consts);
 		cards.add(nutritionPanel, "nutrition");
+		clearHistory(dftm);
 	}
 
-	private ResultSet updateHistory(JTextArea historyText) {
-		historyText.setText("");
-		ResultSet exerciseSet = retrieveHistory(connection, userId);
+	private ResultSet updateHistory(DefaultTableModel table) {
+		table.setRowCount(0);
+		ResultSet exerciseSet = retrieveHistory(connection, userInfo.getUserId());
 		try {
 			while (exerciseSet.next()) {
 				String exerciseType = exerciseSet.getString("exercise_type");
 				int exerciseDuration = exerciseSet.getInt("exercise_time");
 				Date exerciseDate = exerciseSet.getDate("date");
-				String output = exerciseType + " " + exerciseDuration + " " + exerciseDate;
-				historyText.append(output + "\n");
+				Object[] results = { exerciseType, exerciseDuration, exerciseDate };
+				table.addRow(results);
 			}
 
 		} catch (SQLException sqe) {
 			System.out.println("Failed to retrieve history");
 		}
 		return exerciseSet;
+	}
+
+	private void clearHistory(DefaultTableModel table) {
+		JButton clearButton = new JButton();
+		clearButton.setText("Clear All Exercise History");
+		clearButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				SwingUtilities.invokeLater(new Runnable() {
+
+					@Override
+					public void run() {
+						int paneResult = JOptionPane.showOptionDialog(clearButton,
+								"Are you sure you want to clear your history?", "Confirmation",
+								JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+						if (paneResult == JOptionPane.YES_OPTION) {
+							connection.clearHistory(userInfo.getUserId());
+							updateHistory(table);
+						}
+					}
+
+				});
+
+			}
+
+		});
+		this.add(clearButton);
 	}
 
 	/**
